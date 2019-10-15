aecfc08ed95d4660b83a7abda0d0a5157999f0bc
Kathleen Trainor <kathleentrainor9@gmail.com>
2019-10-15 18:46:39 +0200

    Complete stringifyJSON so that it takes all data types  as input and returns a string

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,63 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+
+  function transformToString(obj) {
+  	if (obj === null){
+  		result = result + null;
+  	}
+
+  	else if (typeof obj === 'object') {
+  		if (Array.isArray(obj)) {
+  			result = result + '[';
+  			obj.forEach(function(el) {
+  				transformToString(el);
+  				if (obj.indexOf(el) !== obj.length - 1){
+  					result += ',';
+  				}
+  			});
+  			result = result + ']'
+  		} else {
+  			result = result + '{';
+  			var objSize = 0;
+  			for (var key in obj) {
+  				if (key !== 'functions' && key !== 'undefined'){
+  					transformToString(key);
+  					result += ':';
+  				}
+  				if (obj[key] !== undefined && (!(obj[key] instanceof Function))) {
+  					transformToString(obj[key]);
+  					objSize++;
+  					if(objSize !== Object.keys(obj).length) {
+  						result += ',';
+  					}
+  				}
+  			}
+  			result = result + '}';
+  		}
+  	}
+
+  	else if (typeof obj === 'string'){
+  		result += '"' + obj + '"';
+  	}
+
+  	else if (typeof obj === 'number'){
+  		result += obj.toString();
+  	}
+
+  	else if (typeof obj === 'boolean'){
+  		result += obj;
+  	}
+  }
+  transformToString(obj);
+  return result;
 };
+
+
+console.log(stringifyJSON([
+  {
+    'functions': function() {},
+    'undefined': undefined
+  }
+]));
